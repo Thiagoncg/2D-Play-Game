2fa36125fb7de67fb40689da59db90df3db3e82b
Thiago Garcia <thiagoncg1@gmail.com>
2023-08-25 08:05:10 -0300

    OncollisionEnter
    OncollisionEnter

---
+++ 2D-Player-Game/Assets/UnityCriarGames/Playground/Examples/2dCars/Scripts/PickupController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    //SERIALIZATION VARIABLES
+    [SerializeField]    private Rigidbody2D carBody;
+    [SerializeField]    private Rigidbody2D tireFrontRigdBody;
+    [SerializeField]    private Rigidbody2D tireBackRigdBody;
+    [SerializeField]    private float speed;
+    [SerializeField]    private float speedCar;
+
+    // PRIVATE VARIABLES
+    private float movement;
+
+    private void FixedUpdate() 
+    {
+        MovementACar();
+    }
+
+    private void Update()
+    {
+        GetInputKeyBoard();
+    }
+    
+    //----------------------------------------------------------------
+    private void GetInputKeyBoard()
+    {
+        movement = Input.GetAxis("Horizontal");
+        print("movement" + movement);
+    }
+
+    //----------------------------------------------------------------
+    private void MovementACar()
+    {
+        tireFrontRigdBody.AddTorque(-movement * speed * Time.fixedDeltaTime);                                                    
+        tireBackRigdBody.AddTorque(-movement* speed * Time.fixedDeltaTime);
+        carBody.AddTorque(-movement * speedCar * Time.fixedDeltaTime);
+    }
+
+}
